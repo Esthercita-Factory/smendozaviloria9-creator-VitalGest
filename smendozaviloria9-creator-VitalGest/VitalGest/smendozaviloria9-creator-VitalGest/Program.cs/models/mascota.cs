0eac0412b431a0c89c9fef8fe41283ed37060c3b
sebastian mendoza <smendozaviloria9@gmail.com>
2026-08-28 19:14:03 -0500

    semana 1

---
+++ smendozaviloria9-creator-VitalGest/VitalGest/smendozaviloria9-creator-VitalGest/Program.cs/models/mascota.cs
@@ -0,0 +1,10 @@
+namespace VitalGest.Models
+{
+    public class Mascota
+    {
+        public string Nombre { get; set; }
+        public string Especie { get; set; }
+        public int Edad { get; set; }
+        public string Sintoma { get; set; }
+    }
+}
