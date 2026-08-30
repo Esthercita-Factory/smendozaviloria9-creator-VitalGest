aeb622f766c3314ed51f7d612ca29001ee508f85
sebastian mendoza <smendozaviloria9@gmail.com>
2026-08-30 14:14:03 -0500

    historia de ussuario semana 5

---
+++ smendozaviloria9-creator-VitalGest-ultima-semana/smendozaviloria9-creator-VitalGest/smendozaviloria9-creator-VitalGest/VitalGest/smendozaviloria9-creator-VitalGest/Program.cs/Exceptions/MascotaNoEncontradaException.cs
@@ -0,0 +1,9 @@
+namespace VitalGest.Exceptions
+{
+    public class MascotaNoEncontradaException : Exception
+    {
+        public MascotaNoEncontradaException(string mensaje) : base(mensaje)
+        {
+        }
+    }
+}
