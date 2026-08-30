6b8c4d3de9eb13c0513879986ee90b3474c481cc
sebastian mendoza <smendozaviloria9@gmail.com>
2026-08-30 14:28:33 -0500

    Delete smendozaviloria9-creator-VitalGest-ultima-semana directory

--- smendozaviloria9-creator-VitalGest-ultima-semana/smendozaviloria9-creator-VitalGest/smendozaviloria9-creator-VitalGest/VitalGest/smendozaviloria9-creator-VitalGest/Program.cs/Exceptions/MascotaNoEncontradaException.cs
+++
@@ -1,9 +0,0 @@
-namespace VitalGest.Exceptions
-{
-    public class MascotaNoEncontradaException : Exception
-    {
-        public MascotaNoEncontradaException(string mensaje) : base(mensaje)
-        {
-        }
-    }
-}
